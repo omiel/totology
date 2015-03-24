e834b99582c2d357b3f4fe31121a8c78f849024a
Olivier MIEL <omiel@viadeoteam.com>
2015-03-23 20:41:55 -0700

    prototype

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,62 @@
-console.log("Hello");
+var App = Ember.Application.create({
+  ApplicationAdapter: DS.FixtureAdapter
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('concept');
+  }
+});
+
+App.Concept = DS.Model.extend({
+  preferredLabel: DS.attr('string'),
+  frequency: DS.attr(),
+  path: DS.attr()
+});
+
+App.Concept.reopenClass({
+  FIXTURES: [{
+    id: 1,
+    preferredLabel: 'Java',
+    frequency: 52,
+    path: [{
+      preferredLabel: 'Software development'
+    }, {
+      preferredLabel: 'Programming languages'
+    }]
+  }, {
+    id: 2,
+    preferredLabel: 'JavaScript',
+    frequency: 42,
+    path: [{
+      preferredLabel: 'Software development'
+    }, {
+      preferredLabel: 'Programming languages'
+    }]
+  }, {
+    id: 3,
+    preferredLabel: 'Java',
+    frequency: 2,
+    path: [{
+      preferredLabel: 'Geography'
+    }]
+  }, {
+    id: 4,
+    preferredLabel: 'Java',
+    frequency: 1,
+    path: [{
+      preferredLabel: 'Food and beverages'
+    }, {
+      preferredLabel: 'Hot beverages'
+    }, {
+      preferredLabel: 'Coffee'
+    }]
+  }, {
+    id: 5,
+    preferredLabel: 'Java',
+    frequency: 1,
+    path: [{
+      preferredLabel: 'Dancing style'
+    }]
+  }]
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
-// Gulpfile.js 
+// Gulpfile.js
 var gulp = require('gulp')
+  , bowerFiles = require('main-bower-files')
   , nodemon = require('gulp-nodemon')
-  , inject = require('gulp-inject');
+  , inject = require('gulp-inject')
+  , series = require('stream-series');
 
 gulp.task('inject', function () {
   var target = gulp.src('./client/index.html');
   // It's not necessary to read the files (will speed up things), we're only after their paths:
   var sources = gulp.src(['./client/src/**/*.js', './client/resources/**/*.css'], {read: false});
+  var deps = gulp.src(bowerFiles(), {read: false});
 
-  return target.pipe(inject(sources))
+  return target
+    .pipe(inject(series(deps, sources), { relative: true }))
     .pipe(gulp.dest('./client/.build/'));
 
 });
